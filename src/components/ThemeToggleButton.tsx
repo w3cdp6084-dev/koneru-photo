28ade74bda768cc8b7e0898c5faaf878826649f7
w3cdp6084-dev <yusukew3@gmail.com>
2022-10-11 12:45:49 +0900

    about調整/カラー調整

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -37,7 +37,7 @@ export default function ThemeToggle() {
   }, [])
 
   return isMounted ? (
-    <div className="inline-flex items-center p-[1px] rounded-3xl bg-orange-300 dark:bg-zinc-600">
+    <div className="inline-flex items-center p-[1px] rounded-3xl bg-indigo-600 dark:bg-zinc-600">
       {themes.map(t => {
         const checked = t === theme
         return (
